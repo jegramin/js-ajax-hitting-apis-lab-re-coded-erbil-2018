6a6c523829dd184862ecf3de3de0e6dc9d244aea
Mohammed Ameen <jegroamin@gmail.com>
2018-08-23 11:19:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
-const rootURL = "https://api.github.com"
+const rootURL = "https://api.github.com";
 
-function getRepositories() {
-  const name = document.getElementById("username").value
-  const uri = rootURL + "/users/" + name + "/repos"
-  const xhr = new XMLHttpRequest()
-  xhr.addEventListener("load", displayRepositories)
-  xhr.open("GET", uri)
-  xhr.send()
-  return false;
+function getRepositories(){
+  const name = document.getElementById("username").value;
+  const url = rootURL + "/user/" + name + "/repos";
+  const xhr = new XMLHttpRequest();
+  xhr.addEventListener("load", displayRepositories);
+  xhr.open("GET", url);
+  xhr.send();
 }
-function displayRepositories() {
-  const repos = JSON.parse(this.responseText)
-  const repoList = "<ul>" + repos.map(repo => {
-    const dataUsername = 'data-username="' + repo.owner.login + '"'
-    const dataRepoName = 'data-repository="' + repo.name + '"'
-    return(`
-          <li>
-            <h2>${repo.name}</h2>
-            <a href="${repo.html_url}">${repo.html_url}</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getCommits(this)">Get Commits</a><br>
-            <a href="#" ${dataRepoName} ${dataUsername} onclick="getBranches(this)">Get Branches</a></li>
-          </li>`
-          )
+function displayRepositories(){
+  const repos = JSON.parse(this.responsText);
+  const repoLoist = "<ul>" + repos.map(repo =>{
+    const dataUsername = 'data-username:"' + repo.owner.login +'"';
+    const dataRepository = 'data-repository:"' + repo.name + '"';
+    return(
+      `<li>
+        <h3>${repo.name}</h3>
+        <a href="repo.html_url">${repo.html_url}</a><br>
+        <a href="#" ${dataUsername} ${dataRepository} onclick="getCommits(this)">Get Commits</a><br>
+        <a href="#" ${dataUsername} ${dataRepository} onclick="getBranches(this)">Get Branches</a>
+      </li>`
+    )
   }).join('') + "</ul>";
-  document.getElementById("repositories").innerHTML = repoList
+  document.getElementById(repositories).innerHTML = repoLoist;
 }
