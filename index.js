065d45765405677bcf035b7dd1a088f8f0e021c5
Mohammed Ameen <jegroamin@gmail.com>
2018-08-23 12:28:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -37,9 +37,8 @@ function displayCommits(){
   const commits = JSON.parse(this.responsText);
   const commitList = `<ul>${commits.map(commit => {
     '<li><h3>' + commit.commit.author.name + ' (' + commit.author.login + ')</h3>' + commit.commit.message + '</li>'
-  }).join('') +"</ul>"
-    
-  }`
+  }).join('')
+  } </ul>`
 }
 
 
